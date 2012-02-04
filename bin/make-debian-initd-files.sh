a800798654b5a5ebade36e98ccc91103a3735415
Nick Griffiths <nicobrevin@gmail.com>
2012-02-04 18:12:08 +0000

    pass mode when building example app's initd script

--- bin/make-debian-initd-files.sh
+++ bin/make-debian-initd-files.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-COMMON_PARAMS="--param-app-home=/usr/share/jruby-jsvc/example --param-module-name=Crazy --param-debug=false --param-app-user=root --param-jruby-jsvc-jar=/usr/share/java/jruby-jsvc.jar --param-jsvc=/usr/bin/jsvc --param-jruby-home=/usr/lib/jruby --param-java-home=/usr/lib/jvm/java-6-openjdk"
+COMMON_PARAMS="debian --param-app-home=/usr/share/jruby-jsvc/example --param-module-name=Crazy --param-debug=false --param-app-user=root --param-jruby-jsvc-jar=/usr/share/java/jruby-jsvc.jar --param-jsvc=/usr/bin/jsvc --param-jruby-home=/usr/lib/jruby --param-java-home=/usr/lib/jvm/java-6-openjdk"
 
 NAME=$1
 OUT_TO=$2
